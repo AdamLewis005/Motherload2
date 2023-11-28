272b3d0a943cd8f3fd83c005f2472c59c69915a4
AdamLewis005 <adam.lewis005@gmail.com>
2023-11-28 10:45:59 +0100

    test

--- app/src/main/java/com/example/motherload2/View/MainActivity.kt
+++ app/src/main/java/com/example/motherload2/View/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.motherload2.R
 
-class MainActivity : AppCompatActivity() {// teste git 2
+class MainActivity : AppCompatActivity() {
     private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
